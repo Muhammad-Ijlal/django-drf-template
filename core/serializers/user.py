b28431f32184baa7f2354ed35bb9cf92d6b85929
Muhammad Ijlal <mijlalawan@gmail.com>
2024-08-24 20:00:34 +0400

    Basic dev setup

---
+++ core/serializers/user.py
@@ -0,0 +1,7 @@
+from core.models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.Serializer):
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name']
